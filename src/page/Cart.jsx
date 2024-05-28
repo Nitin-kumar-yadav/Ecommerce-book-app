67665d375471066c262217649cc617ca5226f180
Nitinyadav14920 <nitinkumar94555206@gmail.com>
2024-05-28 22:09:10 +0530

    payment configuration in frontend

--- src/page/Cart.jsx
+++ src/page/Cart.jsx
@@ -5,6 +5,7 @@ import { toast } from 'react-toastify'
 const Cart = () => {
     const dispatch = useDispatch()
     const products = useSelector(state => state.cart)
+    console.log(products.price)
     const handleRemove = (productId) => {
         dispatch(remove(productId))
         toast.warn("Removed product")
@@ -16,6 +17,50 @@ const Cart = () => {
             </div>
         )
     }
+
+    const totalPrice = products.reduce((acc, curr) => acc + curr.price, 0)
+
+    const paymentHandler = async () => {
+        const response = await fetch('http://localhost:5000/books/order', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                amount: totalPrice,
+                currency: 'INR',
+                receipt: 'order_rcptid_11',
+                payment_capture: 1
+            })
+        })
+        const order = await response.json()
+        console.log(order)
+
+        const option = {
+            key: 'rzp_test_1DP5mmOlF5G5ag',
+            amount: totalPrice * 100,
+            currency: 'INR',
+            name: 'Book Store',
+            description: 'Test Transaction',
+            title: 'Payment',
+            image: 'https://i.imgur.com/3g7nmJC.png',
+            order_id: order.id,
+            handler: async function (response) {
+                alert("Transaction Successful", response.razorpay_payment_id);
+            },
+
+            notes: {
+                address: 'Razorpay Corporate Office'
+            },
+            theme: {
+                color: '#F37254'
+            }
+        }
+        const rzp = new window.Razorpay(option)
+        rzp.open()
+    }
+
+
 
     return (
         <div className='cart-item' >
@@ -44,7 +89,10 @@ const Cart = () => {
                 ))
             }
             <div div className="check-out" >
-                <button>Check Out</button>
+                <button onClick={paymentHandler} >Check Out</button>
+                <div div className="total-price" >
+                    <h2>Total Price: ${totalPrice}</h2>
+                </div>
             </div>
         </div >
     )
